8d3c7c8c5bc8d2d139b210e8ab4587490d3b08d7
BryanG26 <bryangonzalez261999@gmail.com>
2024-06-06 11:17:40 -0600

    Rutas de la BD

--- server.js
+++ server.js
@@ -8,7 +8,14 @@ const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
 
-const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000;
+
+export const DB_HOST = process.env.DB_HOST || 'localhost'
+export const DB_USER = process.env.DB_USER || 'root'
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
+export const DB_NAME = process.env.DB_NAME || 'app_tiempos'
+export const DB_PORT = process.env.DB_PORT || '3306'
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
